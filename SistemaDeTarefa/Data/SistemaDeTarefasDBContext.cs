c28f1f76f74393b479fba70f19ed16bcc5cd0a10
Annefrance Costa <annefrance.costa@gmail.com>
2024-06-24 20:23:17 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaDeTarefa/Data/SistemaDeTarefasDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefa.Models;
+
+namespace SistemaDeTarefa.Data
+{
+    public class SistemaDeTarefasDBContext : DbContext
+    {
+        public SistemaDeTarefasDBContext(DbContextOptions<SistemaDeTarefasDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+        public DbSet<TarefaModel> TarefaModels { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ SistemaDeTarefa/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,41 @@
+﻿using SistemaDeTarefa.Data;
+using SistemaDeTarefa.Models;
+using SistemaDeTarefa.Repositorios.Interfaces;
+
+namespace SistemaDeTarefa.Repositorios
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        private readonly SistemaDeTarefasDBContext _dbContext;
+        public UsuarioRepositorio(SistemaDeTarefasDBContext sistemaDeTarefasDBContext)
+        {
+            _dbContext = sistemaDeTarefasDBContext;
+        }
+
+        public Task<UsuarioModel> BuscarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UsuarioModel>> BuscarTodosUsuarios()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<UsuarioModel> Adicionar(UsuarioModel usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Apagar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UsuarioModel> Atualizar(UsuarioModel usuario, int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
